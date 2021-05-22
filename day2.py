e176a5da4756bf5b35eae1fcdce430d5c47d9ab6
Par2002 <84636558+Par2002@users.noreply.github.com>
2021-05-22 23:29:48 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,16 @@
+a='AB de-villiers'
+print(a,type(a))
+b=9577
+print(b,type(b))
+d=53.5
+print(d,type(d))
+e=37
+print(e,type(e))
+
+print(f"My favorite player is {a}. And his ODI runs is {b} ",end=" ")
+print("And his one day international batting avg is {} and his age is {} ".format(d,e))
+
+d2=int(d)
+print(d2,type(d2))
+b1=float(b)
+print(b1,type(b1))
