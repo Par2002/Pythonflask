017af65777ad55a3c27dc83cb305d91aa0366c3e
Par2002 <84636558+Par2002@users.noreply.github.com>
2021-05-25 00:23:14 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,21 @@
+#Day 3
+#Age calculator
+#1)
+print("*********** PART 1 ****************")
+a=int(input("Enter the year of birth:"))
+b=2021
+print("Age of the person is:{}".format(b-a))
+#__________________________________________________________________________
+#Simple calculator
+#2)
+print("*********** PART 2 ****************")
+c=float(input("Enter the first number:"))
+d=float(input("Enter the second number:"))
+print("Addition is:{}".format(c+d))
+print("Subtraction is:{}".format(c-d))
+print("Multiplication is:{}".format(c*d))
+print("Floor division is:{}".format(c//d))
+print("Decimal division is:{}".format(c/d))
+print("Remainder is:{}".format(c%d))
+print("Power of the number is:{}".format(c**d))
+#_____________________________________________________________________________
