12727ac8a701a046d6b888ad7e035d7901726ad7
Par2002 <84636558+Par2002@users.noreply.github.com>
2021-05-26 14:54:29 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,19 @@
+#Day 4
+#Assignment
+#Part 1
+a=str(input("Enter the word: "))
+# print(dir(a))
+print(a.count("y"))
+
+#_____________________________________________
+#Part 2
+b=str(input("Enter the string: "))
+print(b[::2])
+
+#___________________________________________
+#Part 3
+
+c=b=str(input("Enter the string: "))
+print(c.swapcase())
+
+
